11fe1301e4268c176fa8cc7492ae66c624053fb9
Ashwini <ashwinisangale88@gmail.com>
2026-02-11 00:16:13 +0530

    added linkedlist

---
+++ LinkedListDemo/src/com/sample/LinkedListDemo1.java
@@ -0,0 +1,17 @@
+package com.sample;
+
+import java.util.LinkedList;
+
+public class LinkedListDemo1 {
+
+	public static void main(String[] args) {
+		LinkedList<Integer> ll = new LinkedList<Integer> ();
+		ll.add(20);
+		ll.add(30);
+		ll.add(05);
+		ll.add(50);
+		System.out.println(ll);
+
+	}
+
+}
